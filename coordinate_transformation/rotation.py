8c571ad45a922c7e407fbef2b072f6b14215132e
Ethan <ethantao96@gmail.com>
2023-01-05 15:31:22 +0000

    rename 2d rotation

--- coordinate_transformation/rotation.py
+++ coordinate_transformation/rotation.py
@@ -2,8 +2,9 @@
 import numpy as np
 import math
 
-def rotate(point, rotation_degree: float):
+def rotate_2D(point, rotation_degree: float):
     '''
+    SO(2) rotation matrix
     Rotate a point by a given angle in degrees anticlockwise.
     :param point: a np 2D point (x, y)
     :param rotation_degree: angle in degrees
@@ -12,20 +13,10 @@ def rotate(point, rotation_degree: float):
     # check if point is a 2D np point
     if not isinstance(point, np.ndarray) or point.ndim != 1 or point.shape[0] != 2:
         raise ValueError('point must be a 2D numpy point')
-    # rotation_matrix = np.array([[np.cos(np.deg2rad(rotation_degree)), -np.sin(np.deg2rad(rotation_degree))],
-    #                             [np.sin(np.deg2rad(rotation_degree)), np.cos(np.deg2rad(rotation_degree))]])        
-    # return np.dot(rotation_matrix, point)
+    rotation_matrix = np.array([[np.cos(np.deg2rad(rotation_degree)), -np.sin(np.deg2rad(rotation_degree))],
+                                [np.sin(np.deg2rad(rotation_degree)), np.cos(np.deg2rad(rotation_degree))]])        
+    return np.dot(rotation_matrix, point)
 
-    if point[0]==0:
-        
-        ang=float(rotation_degree)+90
-    else:
-        ang = float(rotation_degree+np.arctan(point[1]/point[0])/math.pi*180)
-    dis = np.sqrt(point[0]**2+point[1]**2)
-    points_rotated = np.array([0.0, 0.0])
-    points_rotated[0] = float(dis*np.cos(ang/180*math.pi))
-    points_rotated[1] = dis*np.sin(ang/180*math.pi)
-    return points_rotated
 
 
 
--- coordinate_transformation/test_rotation.py
+++ coordinate_transformation/test_rotation.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import matplotlib.pyplot as plt
 
-from rotation import rotate
+from rotation import rotate_2D
 
 class TestRotation(unittest.TestCase):
 
@@ -10,32 +10,32 @@ class TestRotation(unittest.TestCase):
         rotation_degree = 90
         point_before = np.array([1, 0])
         point_after = np.array([0, 1])
-        self.assertTrue(np.allclose(point_after, rotate(point_before, rotation_degree)))
+        self.assertTrue(np.allclose(point_after, rotate_2D(point_before, rotation_degree)))
 
     def test_rotation_2(self):
         rotation_degree = 60
         point_before = np.array([1, 0])
         point_after = np.array([0.5, np.sqrt(3)/2])
-        self.assertTrue(np.allclose(point_after, rotate(point_before, rotation_degree)))
+        self.assertTrue(np.allclose(point_after, rotate_2D(point_before, rotation_degree)))
 
     def test_rotation_3(self):
         rotation_degree = 360
         point_before = np.array([1, 0])
         point_after = np.array([1, 0])
-        self.assertTrue(np.allclose(point_after, rotate(point_before, rotation_degree)))
+        self.assertTrue(np.allclose(point_after, rotate_2D(point_before, rotation_degree)))
         
     def test_rotation_4(self):
         rotation_degree = 180
         point_before = np.array([0,-1])
         point_after = np.array([0, 1])
-        self.assertTrue(np.allclose(point_after, rotate(point_before, rotation_degree)))
+        self.assertTrue(np.allclose(point_after, rotate_2D(point_before, rotation_degree)))
 
     def test_rotation_5(self):
         # generate a rectangle of 10 points of length 5, width 2
         points = np.array([[0, 0], [5, 0], [5, 2], [0, 2], [0, 0]])
         rotation_angle = 90
         # rotate the rectangle by 45 degrees
-        points_rotated = np.array([rotate(point, rotation_angle) for point in points])
+        points_rotated = np.array([rotate_2D(point, rotation_angle) for point in points])
         
         # plot the rectangle and the rotated rectangle
         plt.plot(points[:, 0], points[:, 1], 'r')
