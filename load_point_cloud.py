210942f2c2bca6159b7cb0b30d2dc3641db2a4f4
Ethan <ethantao96@gmail.com>
2023-01-06 13:37:37 +0000

    create icp class
    contains point2point and point2plane

--- load_point_cloud.py
+++ load_point_cloud.py
@@ -3,22 +3,26 @@ import numpy as np
 
 from viz_plotly import Plotly
 from PointCloudDataLoader import PointCloudDataLoader
+from odometry import ICP
 
 def main():
     # Load point cloud data
-
     current__path = os.path.dirname(os.path.abspath(__file__))
     print('Current folder path is: {}'.format(current__path))
     point_cloud_folder_path = os.path.join(current__path, 'point_cloud_data')
     pc_loader = PointCloudDataLoader(point_cloud_folder_path)
     
 
-    plotly = Plotly()
-    plotly.add_axies()
-    plotly.add_point_cloud(pc_loader.point_clouds[0])
-    # plotly.add_point_cloud(pc_loader.point_clouds[1])
-    plotly.visualise()
-    print('The visualisation is saved to {}'.format(plotly.save_path))
+    # Run ICP
+    old_scan = pc_loader.point_clouds[0]
+    new_scan = pc_loader.point_clouds[1]
+
+    odom =  ICP(old_scan, new_scan)
+    odom.range_filtering(min_range=4, max_range=50)
+    odom.run_icp(method='point_to_plane')
+    odom.run_icp(method='point_to_point')
+
+
 
 if __name__ == '__main__':
     main()
--- util_pc.py
+++ util_pc.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+import open3d as o3d
+
 
 def random_sampling(orig_points, num_points):
     assert orig_points.shape[1] == 3
@@ -18,3 +20,12 @@ def range_filtering(orig_points, min_range, max_range):
     filtered_points = orig_points[mask, :]
 
     return filtered_points
+
+def np_to_o3d_pc(np_array):
+    pc = o3d.geometry.PointCloud()
+    pc.points = o3d.utility.Vector3dVector(np_array)
+    # compute normals
+    pc.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+        radius=0.1, max_nn=30))
+        
+    return pc
